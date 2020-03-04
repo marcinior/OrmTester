344464dd059d319ebbbd593865ea60df2405bf46
Marcin Bobel <marcin.bobel7@gmail.com>
2020-03-04 18:30:05 +0100

    removed unique constraint

---
+++ OrmTester/EntityFramework/Migrations/20200304172822_removeUnique.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFramework.Migrations
+{
+    public partial class removeUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Students_Pesel",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Indexes_IndexNumber",
+                table: "Indexes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Pesel",
+                table: "Students",
+                column: "Pesel",
+                unique: true,
+                filter: "[Pesel] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Indexes_IndexNumber",
+                table: "Indexes",
+                column: "IndexNumber",
+                unique: true);
+        }
+    }
+}
