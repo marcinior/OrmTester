372fc2a63ad8333d37e57bf95938e20989c4cd21
Marcin Bobel <marcin.bobel7@gmail.com>
2020-03-20 20:17:18 +0100

    added indexes
    (cherry picked from commit c4ed560412564bfccb9606e4f60d5f36b2d30fad)

--- OrmTester/NHibernateTester/Entities/StudentMapper.cs
+++ OrmTester/NHibernateTester/Entities/StudentMapper.cs
@@ -8,10 +8,10 @@ namespace NHibernateTester.Entities
         {
             Id(student => student.StudentId);
             Map(student => student.BirthDate).Not.Nullable();
-            References(student => student.ClassId);
+            References(student => student.ClassId).Index("IX_Studnet_ClassId");
             Map(student => student.FirstName).Length(20).Not.Nullable();
             Map(student => student.Gender).Not.Nullable();
-            References(student => student.IndexId).Cascade.Delete();
+            References(student => student.IndexId).Cascade.Delete().Index("IX_Student_IndexId");
             Map(student => student.LastName).Length(45).Not.Nullable();
             Map(student => student.Pesel).Length(11).Nullable();
             Map(student => student.CreatedAt).Not.Nullable();
