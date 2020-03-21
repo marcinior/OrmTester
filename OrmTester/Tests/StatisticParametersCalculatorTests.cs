7784c7c0736da6ef7cc296dfe8338a3b7eef8a5e
Marcin Bobel <marcin.bobel7@gmail.com>
2020-03-21 15:41:43 +0100

    Added property to statistic parameters and increased precision of rounding + unit tests

--- OrmTester/Tests/StatisticParametersCalculatorTests.cs
+++ OrmTester/Tests/StatisticParametersCalculatorTests.cs
@@ -74,7 +74,7 @@ namespace Tests
         {
             StatisticParametersCalculator calculator = new StatisticParametersCalculator(CultureInfo.CurrentCulture);
             double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateAverage", new object[] { integerTestResults });
-            double expected = 3.33;
+            double expected = 3.333;
             Assert.AreEqual(expected, actual);
         }
 
@@ -83,7 +83,7 @@ namespace Tests
         {
             StatisticParametersCalculator calculator = new StatisticParametersCalculator(CultureInfo.CurrentCulture);
             double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateAverage", new object[] { doubleTestResults });
-            double expected = 3.71;
+            double expected = 3.707;
             Assert.AreEqual(expected, actual);
         }
 
@@ -91,7 +91,7 @@ namespace Tests
         [TestCase(4, 3, 1)]
         [TestCase(4, 5, 1)]
         [TestCase(2.32, 2.32, 0)]
-        [TestCase(11.32, 11.39, 0.07)]
+        [TestCase(11.325, 11.392, 0.067)]
         public void CalculateStatisticParameters_DifferenceTest(double average1, double average2, double expected)
         {
             StatisticParametersCalculator calculator = new StatisticParametersCalculator(CultureInfo.CurrentCulture);
@@ -103,7 +103,7 @@ namespace Tests
         public void CalculateStatisticParameters_StandardDeviationTest_IntegerResults()
         {
             StatisticParametersCalculator calculator = new StatisticParametersCalculator(CultureInfo.CurrentCulture);
-            double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateStandardDeviation", new object[] { integerTestResults, 3.33 });
+            double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateStandardDeviation", new object[] { integerTestResults, 3.334 });
             double expected = 1.25;
             Assert.AreEqual(expected, actual);
         }
@@ -112,7 +112,7 @@ namespace Tests
         public void CalculateStatisticParameters_StandardDeviationTest_DoubleResults()
         {
             StatisticParametersCalculator calculator = new StatisticParametersCalculator(CultureInfo.CurrentCulture);
-            double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateStandardDeviation", new object[] { doubleTestResults, 3.71 });
+            double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateStandardDeviation", new object[] { doubleTestResults, 3.707 });
             double expected = 1.06;
             Assert.AreEqual(expected, actual);
         }
@@ -121,17 +121,8 @@ namespace Tests
         public void CalculateStatisticParameters_CoefficentOfVariationt_SingleResults()
         {
             StatisticParametersCalculator calculator = new StatisticParametersCalculator(CultureInfo.CurrentCulture);
-            double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateCoefficentOfVariation", new object[] { 1.25, 3.33 });
-            double expected = 37.54;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void CalculateStatisticParameters_CoefficentOfVariationt_DoubleResults()
-        {
-            StatisticParametersCalculator calculator = new StatisticParametersCalculator(CultureInfo.CurrentCulture);
-            double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateCoefficentOfVariation", new object[] { 1.06, 3.71 });
-            double expected = 28.57;
+            double actual = TestHelper.InvokePrivateMethod(calculator, "CalculateCoefficentOfVariation", new object[] { 1.25, 3.334 });
+            double expected = 37.49;
             Assert.AreEqual(expected, actual);
         }
 
@@ -146,7 +137,7 @@ namespace Tests
                     OperationType = OperationType.Update,
                     IsBulkTest = true,
                     RelationshipType = RelationshipType.OneToMany,
-                    ExecutionTime = TimeSpan.FromMilliseconds(2).AddMicroseconds(7000),
+                    ExecutionTime = TimeSpan.FromMilliseconds(2).AddMicroseconds(7110),
                     NumberOfRecords = 11
                 });
 
@@ -184,23 +175,28 @@ namespace Tests
             };
 
             List<StatisticParameter> results = calculator.CalculateStatisticParameters(efTestResults, nHibernateTestResults);
-            results.Should().ContainSingle(sp => sp.EfAverage == 3.33 &&
+            results.Should().ContainSingle(sp => sp.EfAverage == 3.333 &&
                                             sp.EfStandardDeviation == 1.25 &&
-                                            sp.EfCoefficentOfVariation == 37.54 &&
-                                            sp.NHibernateAverage == 3.67 &&
+                                            sp.EfCoefficentOfVariation == 37.5 &&
+                                            sp.EfExecutionTimePerRecord == 3.333 &&
+                                            sp.NHibernateAverage == 3.667 &&
                                             sp.NHibernateStandardDeviation == 0.47 &&
-                                            sp.NHibernateCoefficentOfVariation == 12.81 &&
-                                            sp.Difference == 0.34 &&
+                                            sp.NHibernateCoefficentOfVariation == 12.82 &&
+                                            sp.NHibernateExecutionTimePerRecord == 3.667 &&
+                                            sp.Difference == 0.334 &&
+                                            sp.NumberOfRecords == 1 &&
                                             sp.TestName == "Single Create 1:1")
-                .And.ContainSingle(sp => sp.EfAverage == 2.7 &&
+                .And.ContainSingle(sp => sp.EfAverage == 2.711 &&
                                             sp.EfStandardDeviation == 0 &&
                                             sp.EfCoefficentOfVariation == 0 &&
-                                            sp.EfExecutionTimePerRecord == 0.25 &&
-                                            sp.NHibernateAverage == 2.11 &&
+                                            sp.EfExecutionTimePerRecord == 0.246 &&
+                                            sp.IsBulk == true &&
+                                            sp.NHibernateAverage == 2.110 &&
                                             sp.NHibernateStandardDeviation == 0 &&
                                             sp.NHibernateCoefficentOfVariation == 0 &&
-                                            sp.Difference == 0.59 &&
-                                            sp.NHibernateExecutionTimePerRecord == 0.19 &&
+                                            sp.Difference == 0.601 &&
+                                            sp.NHibernateExecutionTimePerRecord == 0.192 &&
+                                            sp.NumberOfRecords == 11 &&
                                             sp.TestName == "Bulk Update 1:N");
         }
     }
